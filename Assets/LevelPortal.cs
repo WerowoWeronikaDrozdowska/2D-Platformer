13919446b112613d2ed117ccb4ec1a5ea778984b
WerowoWeronikaDrozdowska <drozdowskaweronika6@gmail.com>
2026-05-11 23:21:29 +0200

    hshshshs

--- Assets/LevelPortal.cs
+++ Assets/LevelPortal.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class SceneOpener : MonoBehaviour
+public class LevelPortal : MonoBehaviour
 {
     public string sceneName;
-   
-    public void OpenScene()
-    {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {   
         SceneManager.LoadScene(sceneName);
+        
+        
     }
 }
