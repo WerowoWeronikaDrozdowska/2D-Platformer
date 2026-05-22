4c6e3a3ff1a261e42bd2582c2a324f0910d2965e
WerowoWeronikaDrozdowska <drozdowskaweronika6@gmail.com>
2026-05-22 21:30:45 +0200

    Piwo

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections;
 public class PlayerMotor : MonoBehaviour
 {
+    public bool canmove = true;
     Vector2 direction;
     private Rigidbody2D rigidbody2D;
     private int canJump = 2;
@@ -38,11 +39,13 @@ public class PlayerMotor : MonoBehaviour
         {
             rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX * stoppingForce, 0));
         }
+      
 
 
     }
     private void Update()
     {
+        animator.SetBool("Isjumping", -rigidbody2D.linearVelocityY > MathF.Abs(rigidbody2D.linearVelocityX));
         animator.SetFloat("Speed",MathF.Abs(rigidbody2D.linearVelocityX));
         if (rigidbody2D.linearVelocityX > 0)
         {
@@ -55,7 +58,7 @@ public class PlayerMotor : MonoBehaviour
     }
     private void OnMove(InputValue value)
     {
-        direction = value.Get<Vector2>();
+        if (canmove)direction = value.Get<Vector2>();
     }
     private void OnSprint()
     {
@@ -63,7 +66,7 @@ public class PlayerMotor : MonoBehaviour
         {
             canDash = false;
             rigidbody2D.AddForce(new Vector2(MathF.Sign(direction.x)*1000, 0));
-            StartCoroutine(InvincibilityTimer(1, Resetdash));
+            StartCoroutine(Timer(1, Resetdash));
         }
 
     }
@@ -71,15 +74,22 @@ public class PlayerMotor : MonoBehaviour
     {
         canDash = true;
     }
-    IEnumerator InvincibilityTimer(float time, Action callback)
+    IEnumerator Timer(float time, Action callback)
     {
         yield return new WaitForSeconds(time);
         callback.Invoke();
     }
+    private void Resetmove()
+    {
+        canmove = true;
+        animator.SetBool("Special", false);
+    }
+    
     private void OnJump()
     {
-        if (canJump>0)
+        if (canJump>0 && canmove)
         {
+            animator.SetBool("Isjumping", true);
             rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             canJump -= 1;
         }
@@ -88,6 +98,15 @@ public class PlayerMotor : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        animator.SetBool("Isjumping", false);
         canJump = 2;
     }
+    private void OnInteract()
+    {
+        canmove = false;
+        Debug.Log("pije piwo");
+        StartCoroutine(Timer(1, Resetmove));
+        animator.SetBool("Special", true);
+    }
+
 }
